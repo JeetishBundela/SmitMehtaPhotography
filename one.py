981bb9ef6c33cbddfa88796fc84cfc3f36b028cf
JeetishBundela <86547840+JeetishBundela@users.noreply.github.com>
2022-06-20 18:36:25 +0530

    Create one.py

---
+++ one.py
@@ -0,0 +1,15 @@
+from random import randint
+
+i=1
+
+
+print(i)
+
+while i!=0:
+    i=randint(100000000000,999999999999)
+    print(i)
+    i += 1
+    if i=100000000010:
+      break
+      
+print("Exit")    
